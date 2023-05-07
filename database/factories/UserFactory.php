8980782116c087fce2fb028519ede0e6894e06b3
Timur <timurcool650@gmail.com>
2023-05-07 20:11:30 +0600

    Remove field 'Email verified at'

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -20,19 +20,8 @@ class UserFactory extends Factory
         return [
             'name' => fake()->name(),
             'email' => fake()->unique()->safeEmail(),
-            'email_verified_at' => now(),
             'password' => \Hash::make('password'), // password
             'remember_token' => Str::random(10),
         ];
-    }
-
-    /**
-     * Indicate that the model's email address should be unverified.
-     */
-    public function unverified(): static
-    {
-        return $this->state(fn (array $attributes) => [
-            'email_verified_at' => null,
-        ]);
     }
 }
