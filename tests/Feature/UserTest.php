1e6f21b16f872cd2b05eebf29c946a8683164f98
Timur <timurcool650@gmail.com>
2023-05-11 09:56:05 +0600

    User tests

--- tests/Feature/UserTest.php
+++ tests/Feature/UserTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature;
 
+use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
@@ -11,12 +12,25 @@ class UserTest extends TestCase
     /**
      * A basic feature test example.
      */
-    public function test_example(): void
+    public function test_show_users_page(): void
     {
+        $this->actingAs(User::find(1));
 
-        $this->assertTrue(true);
-//        $response = $this->get('/');
-//
-//        $response->assertStatus(200);
+        $response = $this->get(route('users.index'));
+
+        $response->assertSuccessful();
+
+        $response->assertViewIs('users.index');
+    }
+
+    public function test_show_user_page(): void
+    {
+        $this->actingAs(User::find(1));
+
+        $response = $this->get(route('users.show', 2));
+
+        $response->assertSuccessful();
+
+        $response->assertViewIs('users.show');
     }
 }
